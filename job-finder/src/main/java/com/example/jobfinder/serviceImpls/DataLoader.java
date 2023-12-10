14871d9abfbdc2ec2019110ef54418a211b390bc
jqzy91 <jamesquek91@live.com.sg>
2023-12-10 20:16:05 +0800

    dataloader v4

--- job-finder/src/main/java/com/example/jobfinder/serviceImpls/DataLoader.java
+++ job-finder/src/main/java/com/example/jobfinder/serviceImpls/DataLoader.java
@@ -1,33 +1,37 @@
 package com.example.jobfinder.serviceImpls;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.jobfinder.entities.Job;
+import com.example.jobfinder.entities.User;
 import com.example.jobfinder.repositories.JobRepository;
+import com.example.jobfinder.repositories.UserRepository;
 
 import jakarta.annotation.PostConstruct;
 
-@Component
 public class DataLoader {
     private JobRepository jobRepository;
+    private UserRepository userRepository;
 
+    @Autowired
     public DataLoader(JobRepository jobRepository) {
         this.jobRepository = jobRepository;
     }
 
-    @PostConstruct
-    public void loadData(){
-        jobRepository.deleteAll();
-
-        // jobRepository.save(new Job("Software Engineer","lorem ipsum", "Information Technology", 4500, 5, "Singapore"));
-
-    // clear the database first
-    // load data here
-    // jobRepository.save(new Job("Analyst"));
-    // jobRepository.save(new Job("Project Manager"));
-    // jobRepository.save(new Job("Scientist"));
-    // jobRepository.save(new Job("Lab Tech"));
-
+    @Autowired
+    public DataLoader(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
+    @PostConstruct
+    public void loadData() {
+        // clear the database first
+        jobRepository.deleteAll();
+        userRepository.deleteAll();
+
+        // load data here
+        jobRepository.save(new Job("Manager", null, null, 0, 0, null));
+
+        userRepository.save(new User("John", "Doe", null, null));
+    }
 }
