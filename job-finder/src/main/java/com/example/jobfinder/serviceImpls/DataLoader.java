80fee586161ef6a3500585f898da453422843aa9
jqzy91 <jamesquek91@live.com.sg>
2023-12-12 08:51:12 +0800

    dataloader v5

--- job-finder/src/main/java/com/example/jobfinder/serviceImpls/DataLoader.java
+++ job-finder/src/main/java/com/example/jobfinder/serviceImpls/DataLoader.java
@@ -30,8 +30,26 @@ public class DataLoader {
         userRepository.deleteAll();
 
         // load data here
-        jobRepository.save(new Job("Manager", null, null, 0, 0, null));
+        jobRepository.save(new Job("Unknown", null, "Operational & Technical", 0, 0, "United Kingdom"));
+        jobRepository.save(new Job("Carpenter", null, "Professional", 0, 0, "Singapore"));
+        jobRepository.save(new Job("Civil Servant", null, "Supervisors & Managers", 0, 0, "Singapore"));
+        jobRepository.save(new Job("Lawyer", null, "Professional", 0, 0, "Singapore"));
+        jobRepository.save(new Job("Fashion Designer", null, "Supervisors & Managers", 0, 0, "Singapore"));
+        jobRepository.save(new Job("Manager", null, "Supervisors & Managers", 0, 0, "Singapore"));
+        jobRepository.save(new Job("Housewife", null, null, 0, 0, "Singapore"));
+        jobRepository.save(new Job("Doctor", null, "Professional", 0, 0, "Singapore"));
+        jobRepository.save(new Job("Student", null, null, 0, 0, "Singapore"));
+        jobRepository.save(new Job("Nurse", null, "Professional", 0, 0, "Singapore"));
 
-        userRepository.save(new User("John", "Doe", null, null));
+        userRepository.save(new User("John", "Doe", "johndoe@gmail.com", "Singapore"));
+        userRepository.save(new User("Gary", "Tay", "garytay@gmail.com", "Singapore"));
+        userRepository.save(new User("David", "Tay", "davidtay@gmail.com", "Singapore"));
+        userRepository.save(new User("Tammy", "Tay", "tammytay@gmail.com", "Singapore"));
+        userRepository.save(new User("Vicky", "Tay", "vickytay@gmail.com", "Singapore"));
+        userRepository.save(new User("Charlie", "Tay", "charlietay@gmail.com", "Singapore"));
+        userRepository.save(new User("Soo Mei", "Tay", "soomeitay@gmail.com", "Singapore"));
+        userRepository.save(new User("Teck Ann", "Tan", "teckanntan@gmail.com", "Singapore"));
+        userRepository.save(new User("Chee Keong", "Tan", "cheekeongtan@gmail.com", "Singapore"));
+        userRepository.save(new User("Shirley", "Ho", "shirleyho@gmail.com", "Singapore"));
     }
 }
