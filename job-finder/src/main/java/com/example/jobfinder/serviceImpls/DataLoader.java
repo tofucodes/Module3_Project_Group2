873a2622dafa2355f6f698327b08c260164fdfd2
jqzy91 <jamesquek91@live.com.sg>
2023-12-13 11:00:49 +0800

    dataloader v6

--- job-finder/src/main/java/com/example/jobfinder/serviceImpls/DataLoader.java
+++ job-finder/src/main/java/com/example/jobfinder/serviceImpls/DataLoader.java
@@ -30,16 +30,16 @@ public class DataLoader {
         userRepository.deleteAll();
 
         // load data here
-        jobRepository.save(new Job("Unknown", null, "Operational & Technical", 0, 0, "United Kingdom"));
-        jobRepository.save(new Job("Carpenter", null, "Professional", 0, 0, "Singapore"));
-        jobRepository.save(new Job("Civil Servant", null, "Supervisors & Managers", 0, 0, "Singapore"));
-        jobRepository.save(new Job("Lawyer", null, "Professional", 0, 0, "Singapore"));
-        jobRepository.save(new Job("Fashion Designer", null, "Supervisors & Managers", 0, 0, "Singapore"));
-        jobRepository.save(new Job("Manager", null, "Supervisors & Managers", 0, 0, "Singapore"));
-        jobRepository.save(new Job("Housewife", null, null, 0, 0, "Singapore"));
-        jobRepository.save(new Job("Doctor", null, "Professional", 0, 0, "Singapore"));
-        jobRepository.save(new Job("Student", null, null, 0, 0, "Singapore"));
-        jobRepository.save(new Job("Nurse", null, "Professional", 0, 0, "Singapore"));
+        jobRepository.save(new Job("Plumber", "Plumbers install, repair and maintain pipes, fixtures and other plumbing equipment used for water distribution and waste water disposal in residential, commercial and industrial buildings.", "Operational & Technical", 45000, 0, "United Kingdom"));
+        jobRepository.save(new Job("Carpenter", "Carpenters construct, install, and repair a variety of residential, commercial, and industrial structures and fixtures.", "Operational & Technical", 50000, 10, "Singapore"));
+        jobRepository.save(new Job("Civil Servant", "Civil servants protect, educate, provide for and help their fellow people.", "Supervisors & Managers", 96000, 8, "Singapore"));
+        jobRepository.save(new Job("Lawyer", "Advise and represent clients in criminal or civil proceedings and in other legal matters.", "Professional", 200000, 4, "Singapore"));
+        jobRepository.save(new Job("Fashion Designer", "Fashion designers sketch designs of clothing, footwear, and accessories.", "Supervisors & Managers", 80000, 6, "Singapore"));
+        jobRepository.save(new Job("Manager", "Maintains staff by recruiting, selecting, orienting, and training employees. Ensures a safe, secure, and legal work environment.", "Supervisors & Managers", 100000, 30, "Singapore"));
+        jobRepository.save(new Job("Housewife", "A housewife (also known as a homemaker) is a woman whose work is running or managing her family's home", null, 0, 30, "Singapore"));
+        jobRepository.save(new Job("Doctor", "Prescribe medications, refer patients to specialized medical professionals and conduct tests to diagnose injuries, illnesses or diseases.", "Professional", 200000, 12, "Singapore"));
+        jobRepository.save(new Job("Student", "Study", null, 0, 0, "Singapore"));
+        jobRepository.save(new Job("Nurse", "Nurses plan and provide medical and nursing care to patients in hospital, at home or in other settings.", "Professional", 90000, 4, "Singapore"));
 
         userRepository.save(new User("John", "Doe", "johndoe@gmail.com", "Singapore"));
         userRepository.save(new User("Gary", "Tay", "garytay@gmail.com", "Singapore"));
